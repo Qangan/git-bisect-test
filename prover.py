ff7c947e5428e6212bef7941c7fc033b33ed7180
Qangan <d.fulman@g.nsu.ru>
2025-02-08 01:30:46 +0700

    Shirley Bassey
    Quote

--- prover.py
+++ prover.py
@@ -2,3 +2,12 @@ def prove(exit_code: int):
     print("Съешь ещё этих мягких французских булок, да выпей чаю")
     exit(exit_code)
 prove(0)
+"""
+Diamonds are forever,
+They are all I need to please me,
+They can stimulate and tease me,
+They won't leave in the night,
+I've no fear that they might
+Desert me.
+		(с) Shirley Bassey
+"""
